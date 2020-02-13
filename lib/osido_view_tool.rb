76bc688625dd66fdee88e33949ca6cb28e4c1791
Dennis Osido <dennisosido@yahoo.com.ph>
2020-02-13 14:27:55 +0800

    View tool initial commit

---
+++ lib/osido_view_tool.rb
@@ -0,0 +1,7 @@
+require "osido_view_tool/version"
+require "osido_view_tool/renderer"
+
+module OsidoViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/osido_view_tool/version.rb
@@ -0,0 +1,3 @@
+module OsidoViewTool
+  VERSION = "0.1.0"
+end
